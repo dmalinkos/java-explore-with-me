d2b31f77e7a3d8fe45b217b44d72229e1c653a3e
dmalinkos <dmalinkos@gmail.com>
2023-07-05 01:23:21 +0300

    minor fixes

--- stats/stats-server/src/main/java/ru/practicum/ewm/mapper/EndpointHitMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/mapper/EndpointHitMapper.java
@@ -1,12 +1,10 @@
 package ru.practicum.ewm.mapper;
 
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.ewm.model.EndpointHit;
 
 @Mapper(componentModel = "spring")
-@Component
 public interface EndpointHitMapper {
 
     EndpointHitDto toDto(EndpointHit endpointHit);
